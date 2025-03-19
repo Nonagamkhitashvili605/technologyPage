748cd04761b52f4a7b3a097b81b22e2733a26b19
nonagamkhitashvili605 <nonagamkhitashvili@gmail.com>
2025-03-19 22:09:17 +0400

    Initial commit

---
+++ technologyPage-main/script.js
@@ -0,0 +1,13 @@
+// ვასელექთებ ბურგერაიქონს,ნავიგაციას
+let navigation = document.getElementById("navigation");
+let burger = document.getElementById("burger");
+// აიქონზე დაკლიკებისას
+burger.addEventListener("click", function () {
+  if (navigation.classList.contains("activeNavigation")) {
+    navigation.classList.remove("activeNavigation");
+    burger.innerHTML = ' <i class="fa-solid fa-bars"></i>';
+  } else {
+    navigation.classList.add("activeNavigation");
+    burger.innerHTML = '<i class="fas fa-times"></i>';
+  }
+});
